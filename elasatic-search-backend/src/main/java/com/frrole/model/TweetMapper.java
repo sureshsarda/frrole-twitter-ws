6a3eebc3cf739ff43ce0cea1a68ab7525bb06684
Suresh Sarda <sureshssarda@gmail.com>
2016-10-09 02:45:29 +0530

    Tweet mapper

---
+++ elasatic-search-backend/src/main/java/com/frrole/model/TweetMapper.java
@@ -0,0 +1,35 @@
+package com.frrole.model;
+
+import java.util.List;
+
+public class TweetMapper {
+
+	public static String asJsonString(List<Tweet> tweets) {
+		StringBuilder sb = new StringBuilder("{\"tweets\": [");
+		for (Tweet tweet : tweets) {
+			sb.append(tweet.asJsonString());
+			sb.append(System.lineSeparator());
+		}
+		sb.append("]}");
+		return sb.toString();
+	}
+
+	public static String asXmlString(List<Tweet> tweets) {
+		StringBuilder sb = new StringBuilder("<Tweets>");
+		for (Tweet tweet : tweets) {
+			sb.append(tweet.asJsonString());
+			sb.append(System.lineSeparator());
+		}
+		sb.append("</Tweets>");
+		return sb.toString();
+	}
+
+	public static String asCsvString(List<Tweet> tweets) {
+		StringBuilder sb = new StringBuilder();
+		for (Tweet tweet : tweets) {
+			sb.append(tweet.asJsonString());
+			sb.append(System.lineSeparator());
+		}
+		return sb.toString();
+	}
+}
